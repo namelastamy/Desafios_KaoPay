7fd2992e3c10db2a21bcfda18856a54c1059f1f1
namelastamy <silvanaomy37@gmail.com>
2023-04-09 17:13:28 -0400

    Desafio005

--- Desafio005.dart
+++ Desafio005.dart
@@ -105,8 +105,27 @@ void main() {
 
   // Lista de álbuns do Spotify
   List<Album> albuns = [
-    Album("Love Yourself: Tear", "51:28", "2018-05-18"),
-    Album("Map of the Soul: 7", "72:08", "2020-02-21"),
+    Album("2 Cool 4 Skool", "20:49", "2013-07-12"),
+    Album("O!RUL8,2?", "30:35", "2013-09-11"),
+    Album("Skool Luv Affair", "35:29", "2014-02-12"),
+    Album("Skool Luv Affair: Special Addition", "43:23", "2014-05-14"),
+    Album("Dark & Wild", "51:31", "2014-08-20"),
+    Album("Wake Up: Standart Edition", "49:39", "2014-12-24"),
+    Album("The Most Beautiful Moment in Life: Pt.1", "31:27", "2015-04-29"),
+    Album("The Most Beautiful Moment in Life: Pt.2", "34:12", "2015-11-30"),
+    Album("The Most Beautiful Moment in Life: Young", "01:28", "2016-05-02"),
+    Album("Youth", "49:16", "2016-09-07"),
+    Album("Wings", "53:41", "2016-10-10"),
+    Album("You Never Walk Alone", "01:06", "2017-02-13"),
+    Album("Love Yourself: 承 Her", "30:42", "2017-09-18"),
+    Album("Face Yourself", "44:25", "2018-04-04"),
+    Album("Love Yourself: 轉 Tear", "43:58", "2018-05-18"),
+    Album("Love Yourself: 結 Answer", "01:44", "2018-08-24"),
+    Album("Map of the soul: Persona", "26:20", "2019-04-12"),
+    Album("Map of the Soul: 7", "01:14", "2020-02-21"),
+    Album("Map of the soul: 7 The Journey", "46:20", "2020-06-14"),
+    Album("Be", "28:30", "2020-11-20"),
+    Album("Proof", "02:80", "2022-06-10"),
   ];
 
   // Instanciando o objeto Spotify
