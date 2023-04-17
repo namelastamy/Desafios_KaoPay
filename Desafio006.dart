6608806a95801a050b9e47b0c85898c1aab0934f
namelastamy <silvanaomy37@gmail.com>
2023-04-17 16:48:07 -0400

    Desafio006

--- Desafio006.dart
+++ Desafio006.dart
@@ -1,66 +1,53 @@
-Class Transacoes {
-     double _debito;
-     double _creditoParcelado;
-     double _pix;
-     String _bandeiraDoCartao;
-     String _nomeDoEstabelecimento;
-     String _nomeDoUsuario;
+ class Transacao {
+    double _debito;
+    double _creditoParcelado;
+    double _pix;
+    String _bandeiraDoCartao;
+    String _nomeDoEstabelecimento;
+    String _nomeDoUsuario;
+    double _dataHora;
 
-    Transacoes(
-        this._debito,
-        this._creditoParcelado,
-        this._pix,
-        this._bandeiraDoCartao,
-        this._nomeDoEstabelecimento,
-        this._nomeDoUsuario);
+    Transacao(this._debito,this._creditoParcelado,this._pix,this._bandeiraDoCartao,this._nomeDoEstabelecimento,this._nomeDoUsuario, this._dataHora);
 
 //Setters e Getters para os atributos privados
-void set debito(double value){
-    _debito = value;
-}
+void set debito(double value){ _debito = value;}
 
 double get debito => _debito;
 
-void set creditoParcelado(double value){
-    _creditoParcelado = value;
-}
+void set creditoParcelado(double value){ _creditoParcelado = value;}
 
 double get creditoParcelado => _creditoParcelado;
 
-void set pix(double value){
-    _pix = value;
-}
+void set pix(double value){ _pix = value;}
 
 double get pix => _pix;
 
-void set bandeiraDoCartao(String value){
-    _bandeiraDoCartao = value;
-}
+void set bandeiraDoCartao(String value){ _bandeiraDoCartao = value;}
 
 String get bandeiraDoCartao => _bandeiraDoCartao;
 
-void set nomeDoEstabelecimento(String value){
-    _nomeDoEstabelecimento = value;
-}
+void set nomeDoEstabelecimento(String value){ _nomeDoEstabelecimento = value;}
 
 String get nomeDoEstabelecimento => _nomeDoEstabelecimento;
 
-void set nomeDoUsuario(String value){
-    _nomeDoUsuario = value;
-}
+void set nomeDoUsuario(String value){ _nomeDoUsuario = value;}
 
 String get nomeDoUsuario => _nomeDoUsuario;
 
+void set dataHora(double value){ _dataHora = value;}
+
+double get dataHora => _dataHora;
+
 }
 
-Class Recebimento extends Transacoes{
-     bool _debitoRecebido;
-     bool _creditoRecebido;
-     bool _pixRecebido;
+class TransacaoRecebimento extends Transacao{
+    bool _debitoRecebido;
+    bool _creditoRecebido;
+    bool _pixRecebido;
 
-     Recebimento(
+     TransacaoRecebimento(
         double debito, 
-        double creditoParceldo, 
+        double creditoParcelado, 
         double pix, 
         String bandeiraDoCartao, 
         String nomeDoEstabelecimento, 
@@ -68,24 +55,18 @@ Class Recebimento extends Transacoes{
         this._debitoRecebido, 
         this._creditoRecebido, 
         this._pixRecebido)
-     :super(debito, creditoParcelado, pix, bandeiraDoCartao, nomeDoEstabelecimento, nomeDoUsuario);
+     :super(debito, creditoParcelado, pix, bandeiraDoCartao, nomeDoEstabelecimento, nomeDoUsuario, dataHora);
 
 //Setters e Getters para os atributos privados
-void set debitoRecebido(bool value){
-    _debitoRecebido = value;
-}
+void set debitoRecebido(bool value){ _debitoRecebido = value;}
 
 bool get debitoRecebido => _debitoRecebido;
 
-void set creditoRecebido(bool value){
-    _creditoRecebido = value;
-}
+void set creditoRecebido(bool value){ _creditoRecebido = value;}
 
 bool get creditoRecebido => _creditoRecebido;
 
-void set pixRecebido(bool value){
-    _pixRecebido = value;
-}
+void set pixRecebido(bool value){ _pixRecebido = value;}
 
 bool get pixRecebido => _pixRecebido;
 }
@@ -93,131 +74,116 @@ bool get pixRecebido => _pixRecebido;
 class Wallet {
   String _id;
   double _saldo;
-  List<Transacao> transacoes; //lista para armazenar as transações realizadas
+  List<Transacao> _transacoes; //lista para armazenar as transações realizadas
   
-  Wallet(
-    this._id, 
-    this._saldo){this.transacoes = []; //inicializa a lista de transacoes vazia
-    }
-
+  Wallet(this._id, this._saldo) : _transacoes = []; //inicializa a lista de transacoes vazia
+    
 //Setters e Getters para os atributos privados
-void set id(String value){
-    _id = value;
-}
-
-String get id => _id;
-
-void set saldo(double value){
-    _saldo = value;
-}
-
 double get saldo => _saldo;
 
-void pagarDebito(double value){
-    if(value > _saldo){
+void pagarDebito(double value, String bandeiraDoCartao, String nomeDoEstabelecimento, String nomeDoUsuario){
+    if(value > _saldo) {
         print("Saldo Insuficiente para Realizar a Transação");
-        Return;
+        return;
     }
-
     _saldo -= value;
+    _transacoes.add(Transacao(value,0,0,'Visa','Supermercado DB','Maria Silva'));
 }
 
-void pagarCredito(double value){
+void pagarCredito(double value, String bandeiraDoCartao, String nomeDoEstabelecimento, String nomeDoUsuario){
     if(value > _saldo){
         print("Saldo Insuficiente para Realizar a Transação");
         return;
     }
-
     _saldo -= value;
+    _transacoes.add(Transacao(value,0,0,'Elo','Amanda Beauty','Jeon Sulmi'));
 //armazena as informaçoes adicionais da transação 
 }
 
-void pagarPix(double value){
+void pagarPix(double value, String nomeDoEstabelecimento, String nomeDoUsuario){
     _saldo -= value;
+    _transacoes.add(Transacao(value,28,value,'MasterCard','Uber','KaoPay'));
     //armazena as informações adicionais da transação
-    var transacao = Transacao(DateTime.now(),value);
-    transacoes.add(transacao);
 }
 
 void pagarCreditoParcelado(double value, int numParcelas, double taxa){
     if(value > _saldo){
         print("Saldo Insuficiente para Realizar a Transação.");
-        Return;
+        return;
     }
-
     _saldo -= value;
 //divide o valor em um número de parcelas e armazena as informaçoes adicionais da transação 
-   var valorParcela = value / numParcelas;
+   final valorParcela = value / numParcelas;
     for (var i = 1; i <= numParcelas; i++) {
-      var data = DateTime.now().add(Duration(days: 30 * i));
-      var transacao = TransacaoCreditoParcelado(data, valorParcela, numParcelas, taxa);
-      transacoes.add(transacao);
+      final data = DateTime.now().add(Duration(days: 30 * i));
+      _transacoes.add(Transacao(valorParcela, valorParcela * (1 + taxa),0,'','','','','',' b  '));
     }
  }
 
- void listarTransacoes() {
-    for (var transacao in transacoes) {
-      print('Data: ${transacao.data}');
-      print('Hora: ${transacao.hora}');
-      print('Valor: ${transacao.valor}');
-
-      if (transacao is TransacaoCreditoParcelado) {
-        print(
-            'Informações adicionais: ${transacao.numParcelas} parcelas com taxa de ${transacao.taxa}');
-      }
-    }
+void adicionarTransacao(Transacao transacao) {
+    _transacoes.add(transacao);
   }
-}
 
-//classe transação para representar uma transação
-class Transacao{
-    DateTime data;
-    double value;
-    String hora;
+void listarTransacoes() {
+     void listarTransacoes() {
+    for (var i = 0; i < _transacoes.length; i++) {
+      print('Data e hora da transação: ${_transacoes[i].dataHora}');
+      print('Bandeira do cartão: ${_transacoes[i].bandeiraDoCartao}');
+      print('Nome do estabelecimento: ${_transacoes[i].nomeDoEstabelecimento}');
+      print('Nome do usuário: ${_transacoes[i].nomeDoUsuario}');
+      print('Valor da transação: ${_transacoes[i].valor}');
 
-    Transacao(this.data,this.value){
-        this.hora ='${data.hour}:${data.minute}';
+      if (_transacoes[i] is credito) {  
+        print('Tipo de transação: Crédito');
+      } else if (_transacoes[i] is debito) {
+        print('Tipo de transação: Débito');
+      } else if (_transacoes[i] is creditoParcelado) {
+        print('Tipo de transação: Crédito Parcelado');
+      } else if (_transacoes[i] is pix) {
+        print('Tipo de transação: PIX');
+      }
+
+      print('');
+    
     }
+   }
+ }
 }
 
 class User {
-  String _name;
-  String _phone;
-  String _cpf;
-  Wallet _carteira;
+  final String _name;
+  final String _phone;
+  final String _cpf;
+  final Wallet _carteira;
   
-  User(
-    this._name, 
-    this._phone, 
-    this._cpf,
-    this._carteira);
+  User(this._name, this._phone, this._cpf,this._carteira);
 
 //Setters e Getters para os atributos privados
-void set name(String value){
+void set _name(String value){
     _name = value;
 }
 
 String get name => _name;
 
-void set phone(String value){
+void set _phone(String value){
     _phone = value;
 }
 
 String get phone => _phone;
 
-void set cpf(String value){
+void set _cpf(String value){
     _cpf = value;
 }
 
 String get cpf => cpf;
 
-Wallet get carteira => _carteira
+Wallet get carteira => _carteira;
 }
 
 void main(){
 
 //instancia da carteira com ID unico
-var carteira = Wallet(id: '123456789');
+var carteira = Wallet(id: '123456789676898');
 
 //instancia da class User
 var user = User(name: 'Naomy', phone: '123456789', cpf: '12345678901');
